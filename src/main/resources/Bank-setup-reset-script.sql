39989de32cc1e35978d97ea537363817a6a31a8d
RileyJatRevature <riley593@revature.com>
2024-07-15 11:12:07 -0400

    Began implementing join table

--- src/main/resources/Bank-setup-reset-script.sql
+++ src/main/resources/Bank-setup-reset-script.sql
@@ -1,5 +1,6 @@
 drop table if exists users;
 drop table if exists accounts;
+drop table if exists user_account_join;
 
 CREATE table users(
     user_id integer PRIMARY KEY autoincrement,
@@ -7,20 +8,29 @@ CREATE table users(
 	password text
 );
 
-INSERT into users(username, password) values ('admin', '1234');
-INSERT INTO users(username, password) VALUES ('LetsHoldUp', 'TheBestPasswordEver');
-
 CREATE table accounts(
 	account_id integer PRIMARY KEY autoincrement,
-	user_id integer NOT NULL REFERENCES users(user_id),
 	account_type text DEFAULT('Checking'),
 	account_nickname text,
 	account_balance float DEFAULT(0.0)
 );
 
-INSERT INTO accounts(user_id, account_nickname) VALUES (1, 'testing account');
-INSERT INTO accounts(user_id, account_balance) VALUES (2, 1000000.43);
-INSERT INTO accounts(user_id, account_nickname, account_balance) VALUES (1, 'Admin''s real account don''t delete', 3.50);
-INSERT INTO accounts(user_id, account_balance) VALUES (1, 1000000.43);
+CREATE TABLE user_account_join(
+    user_id integer NOT NULL REFERENCES users(user_id),
+    account_id integer NOT NULL REFERENCES accounts(account_id)
+);
 
+INSERT into users(username, password) values ('admin', '1234');
+INSERT INTO users(username, password) VALUES ('LetsHoldUp', 'TheBestPasswordEver');
+
+INSERT INTO accounts(account_nickname) VALUES ('testing account');
+INSERT INTO accounts(account_balance) VALUES (1000000.43);
+INSERT INTO accounts(account_nickname, account_balance) VALUES ('Admin''s real account don''t delete', 3.50);
+INSERT INTO accounts(account_balance) VALUES (1000000.43, 'Joint account test');
+
+INSERT INTO user_account_join(1, 1);
+INSERT INTO user_account_join(1, 3);
+INSERT INTO user_account_join(1, 4);
+INSERT INTO user_account_join(2, 2);
+INSERT INTO user_account_join(2, 4);
 
